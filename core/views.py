839af92b142e1a9f0741806317e043875ec550e1
gns0304 <gns0304@naver.com>
2021-08-07 02:55:41 +0900

    [EDIT] serializers.py and views.py in core

--- core/views.py
+++ core/views.py
@@ -1,8 +1,57 @@
-from django.shortcuts import render
+import datetime
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import *
 from .tasks import dowith_duhee
-# Create your views here.
+
+
+class ChallengeView(APIView):
+
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request):
+
+        gathering_challenge = Challenge.objects.filter(start_date__gt=datetime.date.today())
+        gathering_serializer = ChallengeSerializer(gathering_challenge, many=True)
+
+        ongoing_challenge = Challenge.objects.filter(start_date__lte=datetime.date.today(), end_date__gte=datetime.date.today())
+        ongoing_serializer = ChallengeSerializer(ongoing_challenge, many=True)
+
+        complete_challenge = Challenge.objects.filter(end_date__lt=datetime.date.today())
+        complete_serializer = ChallengeSerializer(complete_challenge, many=True)
+
+        return_data = {
+            "gathering": gathering_serializer.data,
+            "ongoing": ongoing_serializer.data,
+            "complete": complete_serializer.data
+        }
+
+        return Response(return_data)
+
+    def post(self, request):
+
+        serializer = ChallengeSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            if not request.user.is_authenticated:
+                return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
+
+            serializer.save(captain=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChallengeDetailView(APIView):
+    def get(self, request, pk):
+        pass
 
 def dowith_celery(request):
     dowith_duhee.delay('이렇게도 되고')
     dowith_duhee.apply_async(('expires 는 선택', ), expires=600)
     return None
+
