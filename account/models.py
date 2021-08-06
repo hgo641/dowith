c0afa01e8557a8a8bf5197209a2d1bbdef0b8eee
gns0304 <gns0304@naver.com>
2021-08-06 16:51:53 +0900

    edit user model (add point field)

--- account/models.py
+++ account/models.py
@@ -44,6 +44,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     nickname = models.CharField(max_length=10, null=False, blank=False)
     registered_on = models.DateTimeField(auto_now_add=True)
     image_url = models.URLField(null=True, blank=True)
+    point = models.PositiveIntegerField(null=False, blank=False, default=5000)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
--- account/views.py
+++ account/views.py
@@ -1,10 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
 # Create your views here.
 
-
-def tst(request):
-    user = User.objects.create_user('naver', '123', 'nick')
-    user.save()
-    return redirect('/')
