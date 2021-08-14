3457cd150ab15a2d6baa5ee786a2524646fc281a
sanvit <sanvit@sanvit.me>
2021-08-14 01:02:30 +0000

    change cdn url

--- dowith/settings.py
+++ dowith/settings.py
@@ -163,7 +163,7 @@ AWS_SECRET_ACCESS_KEY = 'uCAcPlRDDpCxosIo7JxzIg+CvGPIWNxxDF27eoQw'
 # S3 File Storage
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_STORAGE_BUCKET_NAME = 'likelion-django-lesson'
-AWS_S3_CUSTOM_DOMAIN = 'd1f5v0bury73ux.cloudfront.net'
+AWS_S3_CUSTOM_DOMAIN = 'dowith-cdn.jaewon.dev'
 
 # CORS Allow All
 CORS_ORIGIN_ALLOW_ALL = True
